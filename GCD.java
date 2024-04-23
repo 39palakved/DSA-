6df313f81422dcc38be65901bb9f1f962d6b5d2f
palak porwal <99019333+39palakved@users.noreply.github.com>
2024-04-23 22:51:24 +0530

    Add files via upload

--- GCD.java
+++ GCD.java
@@ -1,29 +1,12 @@
-class Solution
-{
-    public int gcd(int N , int arr[]) 
-    { 
-       //code here.
-       if(arr.length==1){
-          return arr[0] ;
-       }
-       Arrays.sort(arr);
-       int n=arr[0];
-      
-       
-       while(n>=1){
-            int temp=0;
-           for(int i=0; i<N;i++){
-               if(arr[i]%n==0){
-                  temp++;
-               }
-               
-           }
-           if(temp==N){
-               return n;
-           }
-           
-           n--;
-       }
-       return 1;
-    } 
+class Solution {
+    public static int gcd(int a, int b) {
+        // code here
+        while (b!=0){
+            int temp=b;
+            b=a%b;
+            a=temp;
+        }
+        return a;
+    }
 }
+      
